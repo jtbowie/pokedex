3068b88de8742333f75aab011c0845e35f4eb815
Jon Bowie <jon.bowie@gmail.com>
2025-04-15 22:28:37 -0400

    created initial REPL loop

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"reflect"
 	"strings"
 )
+
+const PLACE_CURSOR string = "\033[H\033[3J\033[80;1H"
+
+func clearScreen() {
+	fmt.Fprintf(os.Stdout, PLACE_CURSOR)
+}
 
 func cleanInput(text string) []string {
 	cleanTextSplit := strings.Split(strings.ToLower(strings.TrimSpace(text)), " ")
@@ -13,6 +22,20 @@ func cleanInput(text string) []string {
 	return []string{}
 }
 
+func replLoop() {
+	var line, word string
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Printf("PODEX9001 > ")
+		scanner.Scan()
+		line = scanner.Text()
+		word = strings.Split(line, " ")[0]
+		fmt.Printf("Your command was: %s\n", strings.ToLower(word))
+	}
+}
+
 func main() {
-	fmt.Println(cleanInput("Hello, World!"))
+	clearScreen()
+	fmt.Printf("%s\n", reflect.TypeOf(os.Stdin))
+	replLoop()
 }
