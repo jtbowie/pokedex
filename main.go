b7931deedd27b6a936d4eb68b4e95d2763871ad4
Jon Bowie <jon.bowie@gmail.com>
2025-04-16 01:45:07 -0400

    added map and mapb commands for location area pull

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -13,6 +16,19 @@ type cliCommand struct {
 	description string
 	callBack    func() error
 }
+
+type locationArea struct {
+	Count   int    `json:"count"`
+	Next    string `json:"next"`
+	Prev    string `json:"previous"`
+	Results []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+var currentMapUrl = ""
+var currentLocationArea locationArea
 
 const PLACE_CURSOR string = "\033[H\033[3J\033[80;1H"
 
@@ -34,6 +50,80 @@ func commandHelp() error {
 	return nil
 }
 
+func commandMapB() error {
+	if currentMapUrl == "null" {
+		fmt.Println("Final page reached")
+		return nil
+	}
+
+	currentLocationArea, err := fillLocationArea(currentLocationArea.Prev)
+
+	if err != nil {
+		fmt.Println("Error encountered retrieving locations... Exiting gracefully.")
+		commandExit()
+	}
+
+	for result := range len(currentLocationArea.Results) {
+		fmt.Println(currentLocationArea.Results[result].Name)
+	}
+
+	return nil
+}
+
+func commandMap() error {
+	if currentMapUrl == "null" {
+		fmt.Println("Final page reached")
+		return nil
+	}
+
+	currentLocationArea, err := fillLocationArea(currentMapUrl)
+
+	if err != nil {
+		fmt.Println("Error encountered retrieving locations... Exiting gracefully.")
+		commandExit()
+	}
+
+	for result := range len(currentLocationArea.Results) {
+		fmt.Println(currentLocationArea.Results[result].Name)
+	}
+
+	return nil
+}
+
+func parseLocationJSON(res http.Response) (locationArea, error) {
+	body, err := io.ReadAll(res.Body)
+	defer res.Body.Close()
+
+	var locationAreas locationArea
+	err = json.Unmarshal(body, &locationAreas)
+	if err != nil {
+		return locationArea{}, errors.New("Invalid JSON returned")
+	}
+
+	return locationAreas, nil
+}
+
+func fillLocationArea(url string) (locationArea, error) {
+	if url == "" {
+		url = "https://pokeapi.co/api/v2/location-area/"
+	}
+
+	res, err := http.Get(url)
+
+	if err != nil {
+		return locationArea{}, errors.New("Error connecting to endpoint.")
+	}
+
+	locationAreas, err := parseLocationJSON(*res)
+	if err != nil {
+		return locationArea{}, errors.New("JSON Parsing failed")
+	}
+
+	currentMapUrl = locationAreas.Next
+
+	return locationAreas, nil
+}
+
 func clearScreen() {
 	fmt.Fprintf(os.Stdout, PLACE_CURSOR)
 }
@@ -52,6 +142,8 @@ func buildCommandHooks(rawHooks map[string]cliCommand) {
 	}
 	rawHooks["exit"] = cliCommand{name: "exit", description: "Exit the Pokedex", callBack: commandExit}
 	rawHooks["help"] = cliCommand{name: "help", description: "Displays a help message", callBack: commandHelp}
+	rawHooks["map"] = cliCommand{name: "map", description: "Display the next location areas", callBack: commandMap}
+	rawHooks["mapb"] = cliCommand{name: "mapb", description: "Display the prev location areas", callBack: commandMapB}
 }
 
 func replLoop() {
